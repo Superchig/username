5d63502e53c268313e982df265e912175e57495d
Christopher Chang <chiggiechang@gmail.com>
2019-11-04 10:31:58 -0800

    Changes the regex in username.sh to function (hopefully) correctly

--- username.sh
+++ username.sh
@@ -4,18 +4,18 @@
 
 echo "Username:"
 echo "The only characters that can be used are"
-echo "\tlower case letters,"
-echo "\tdigits, and"
-echo "\tthe underscore character."
+echo -e "\tlower case letters,"
+echo -e "\tdigits, and"
+echo -e "\tthe underscore character."
 echo "It must start with a lower case letter."
 echo "It must contain at least three but no more than 12 characters."
-echo -n "Enter username: "
+echo "Enter username:"
 read NAME
 
-while echo "$NAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | egrep -v "^[a-z]([0-9]|[a-z]|_){2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username! Remember the rules."
-	echo -n "Enter a valid username: "
+	echo "Enter a valid username:"
 	read NAME
 done
 echo "Thank you"
